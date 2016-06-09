a187bb09e540c0ddec7f4a48ce3da547b0186723
len <len@debian>
2016-06-09 23:19:20 +0200

    Projekt wraz z dokumentacją

--- Epidemia/Country.h
+++ Epidemia/Country.h
@@ -19,8 +19,8 @@ public:
 	int persons(); /** *Zwraca rozmiar wektora osób */
 	int infectedPersons();/** *Zwraca rozmiar wektora osób zainfekowanych */
 	int cure();/** *Zwraca wartość postępu szukania lekarstwa (od 10000 do 0)*/
-	void InfectPerson(std::vector<Person *>::iterator personOpponent);/** *W przypadku walki wygranej przez chorobę, funcja ta zaraża człowieka chorobą z którą przegrał walkę.
-	void fight(fstream &output, int outputInfo);//Symuluje walkę pomiędzy osobą a chorobą. Choroba nie zadaje osobie żadnych obrażeń, w przypadku wygranej po prostu zaraża osobę, natomiast w przypadku przegranej jednostka tej choroby ginie. Jeśli wygrywa osoba, to nie zostaje zarażona. Wynik walki jest uzależniony od parametrów zarówno choroby, jak i osoby, wybieranych losowo, jednak z innym przedziałem losowości w przypadku różnych klas chorób i osób. */
+	void InfectPerson(std::vector<Person *>::iterator personOpponent);/** *W przypadku walki wygranej przez chorobę, funcja ta zaraża człowieka chorobą z którą przegrał walkę. */
+	void fight(fstream &output, int outputInfo);/** *Symuluje walkę pomiędzy osobą a chorobą. Choroba nie zadaje osobie żadnych obrażeń, w przypadku wygranej po prostu zaraża osobę, natomiast w przypadku przegranej jednostka tej choroby ginie. Jeśli wygrywa osoba, to nie zostaje zarażona. Wynik walki jest uzależniony od parametrów zarówno choroby, jak i osoby, wybieranych losowo, jednak z innym przedziałem losowości w przypadku różnych klas chorób i osób. */
 	void spreadVirus(int i, fstream &output);/** *Zaraża losowo wybraną osobę co 4 dni. Czyli symulacja naturalnego rozprzestrzeniania sie wirusa */
 	bool searchForCure(fstream &output);/** *Naukowcy szukają lekarstwa. Powodzenie poszukiwań zależne jest od wartości ich inteligencji, a jego wartość jest losowana pod koniec każdego dnia i odejmowana od puli (10000 to początkowa wartość puli) */
 	void cureSomeInfectedPersons(fstream &output);/** *Jeśli wartość _cure będzie równa 0 funkcja ta zaczyna leczyć ludzi. Lekarstwo jest losowo rozpraszane na kilka (ilość losowa) jednostek, a każda z nich ma 50% szans na powrót do zdrowia */
